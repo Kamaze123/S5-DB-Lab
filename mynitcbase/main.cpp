3a2791c78440a83fe6b84b136351d827b8725cbf
Kamalesh <kamaleshbabu622@gmail.com>
2026-08-04 11:46:15 +0530

    second stage

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -14,20 +14,43 @@
 //   //return FrontendInterface::handleFrontend(argc, argv);
 // }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]){
   Disk disk_run;
 
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 7000);
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
-  Disk::writeBlock(buffer, 7000);
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  std::cout << message2 << "\n";
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for(int i = 0; i < relCatHeader.numEntries; i++){
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS];
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for(int j = 0; j < attrCatHeader.numEntries; j++){
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, j);
+
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0){
+        const char *attrType;
+        if ((int)attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER) {
+          attrType = "NUM";
+        }else{
+          attrType = "STR";
+        }
+
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
+    printf("\n");
+  }
 
   return 0;
 }
